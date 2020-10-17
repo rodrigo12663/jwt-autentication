a03df427172c16b08627b2ef0c7cd645f53011c6
rodrigo12663 <ricardodedetizadora@hotmail.com>
2020-10-16 23:30:02 -0300

    middleware auth

--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -1,14 +1,17 @@
 const jwt = require("jsonwebtoken");
-const authConfig = require ("../../config/auth.json")
+const authConfig = require ("../../config/auth.json");
 function auth(req,res,next){
     const authToken = req.headers.authorization;
+    const [, token] = authToken.split(" ");
 
+    
+    console.log(token);
     if(!authToken ){
         res.status(401).json({error:"token invalid"});
     }
-    jwt.verify(authToken,authConfig.secret,(err,data)=>{
+    jwt.verify(token,authConfig.secret,(err,data)=>{
         if(err){
-            res.status(401).json({error:"token invalid"});
+            res.status(401).json({err:"token invalid"});
         }
         else{
             req.token= authToken;
@@ -16,7 +19,5 @@ function auth(req,res,next){
             next();
         }
     });
-
 }
-
 module.exports =auth;
